e433cabb671f5870a862b622cbc14d3f2793c741
Stereopapa <72004744+Stereopapa@users.noreply.github.com>
2026-02-26 19:01:08 +0100

    added few more rules for tests in gitignore

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -2,23 +2,29 @@ from experiment.experiment_data import ExperimentDegrees,ExperimentRecords
 from experiment.tree_experiment_runner import TreeExperimentRunner
 from unittest.mock import patch
 
+@patch("experiment.tree_experiment_runner.TreeExperimentRunner.RESULT_CHART_PATH_STR", "./tests/docs/charts")
+@patch("experiment.tree_experiment_runner.TreeExperimentRunner.RESULT_TABLE_PATH_STR", "./tests/docs/tables")
 @patch("experiment.tree_experiment_runner.TreeExperimentRunner.TEMP_PATH_STR", "./tests/experiment/table/temp")
 @patch("experiment.tree_experiment_runner.TreeExperimentRunner.MAIN_PATH_STR", "./tests/experiment/table/main")
 def test_experiment_records():
     experiment = TreeExperimentRunner()
+    experiment.save_chart_and_table = True
     experiment.run_experiment_records(ExperimentRecords.DEFAULT)
-    experiment.print_chart()
 
+@patch("experiment.tree_experiment_runner.TreeExperimentRunner.RESULT_CHART_PATH_STR", "./tests/docs/charts")
+@patch("experiment.tree_experiment_runner.TreeExperimentRunner.RESULT_TABLE_PATH_STR", "./tests/docs/tables")
 @patch("experiment.tree_experiment_runner.TreeExperimentRunner.TEMP_PATH_STR", "./tests/experiment/table/temp")
 @patch("experiment.tree_experiment_runner.TreeExperimentRunner.MAIN_PATH_STR", "./tests/experiment/table/main")
 def test_experiment_degrees():
     experiment = TreeExperimentRunner()
+    experiment.save_chart_and_table = True
     experiment.run_experiment_degrees(ExperimentDegrees.DEFAULT)
-    experiment.print_chart()
 
+@patch("experiment.tree_experiment_runner.TreeExperimentRunner.RESULT_CHART_PATH_STR", "./tests/docs/charts")
+@patch("experiment.tree_experiment_runner.TreeExperimentRunner.RESULT_TABLE_PATH_STR", "./tests/docs/tables")
 @patch("experiment.tree_experiment_runner.TreeExperimentRunner.TEMP_PATH_STR", "./tests/experiment/table/temp")
 @patch("experiment.tree_experiment_runner.TreeExperimentRunner.MAIN_PATH_STR", "./tests/experiment/table/main")
 def test_experiment_degrees_reverse():
     experiment = TreeExperimentRunner()
+    experiment.save_chart_and_table = True
     experiment.run_experiment_degrees(ExperimentDegrees.REVERSE_DEFAULT)
-    experiment.print_chart()
